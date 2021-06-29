fc9d3bc38e7b77b187131cbb7defce3cebedf1f6
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-06-28 22:47:32 -0500

    reframe bindings

--- resources/snippets/repl/appdb.clj
+++ resources/snippets/repl/appdb.clj
@@ -1,6 +1,11 @@
 {:state 42
  :html  [:div
-         "app db: " (pr-str (app-db))]
+         [:button.bg-blue-300.m-2 {:on-click #(rf/dispatch [:bidi/goto :goldly/about])}
+          "goto main"]
+
+         "app db: "
+         [:p.bg-blue-300
+          (pr-str (app-db))]]
  :fns {}}
 
 
