c37660fc4c0f844725429ddf80f8eb85d20c003b
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-06-30 17:01:37 -0500

    remove old css system

--- src/pinkgorilla/repl/cljs/core.cljs
+++ src/pinkgorilla/repl/cljs/core.cljs
@@ -1,8 +1,6 @@
 (ns pinkgorilla.repl.cljs.core
   (:require
-   ;[re-frame.core :as rf]
    ;[pinkie.pinkie :refer-macros [register-component]]
-   ;[ui.site.goldly.css :as css]
    [pinkgorilla.repl.cljs.webly]
    [pinkgorilla.repl.cljs.goldly]
    [pinkgorilla.repl.cljs.js]
@@ -11,7 +9,3 @@
    [pinkgorilla.repl.cljs.http]
    [pinkgorilla.repl.cljs.reframe]
    [pinkgorilla.repl.clipboard]))
-
-;(register-component :p/markdown markdown-viewer)
-;(rf/dispatch [:css/add-components css/components css/config])
-
