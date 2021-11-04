95450eca6a7467f4fae032b27e35f6170b856118
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-11-04 02:18:23 +0100

    tick alpha dep bump

---
+++ src/demo/notebook/dialog.clj
@@ -0,0 +1,18 @@
+{:state 42
+ :html  [:div
+
+         [:input.m-2 {:type "button"
+                      :value "Click me"
+                      :on-click #(notify "booom")}]
+
+         [:input.m-2 {:type "button"
+                      :value "show dialog"
+                      :on-click #(dialog [:h1.bg-blue-300.p-5 "dummy dialog"])}]
+
+         [:input.m-2 {:type "button"
+                      :value "goto main"
+                      :on-click #(nav :goldly/about)}]]
+
+ :fns {}}
+
+
