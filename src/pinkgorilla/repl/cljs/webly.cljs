ba1fecf363e3554e0f90565533d0c6d272fe7c0d
awb99 <awb99@gmail.com>
2023-08-12 12:37:08 -0500

    removed ui-bidi related stuff

--- src/pinkgorilla/repl/cljs/webly.cljs
+++ src/pinkgorilla/repl/cljs/webly.cljs
@@ -1,17 +1,10 @@
 (ns pinkgorilla.repl.cljs.webly
   (:require
    [re-frame.core :as rf]
-   [frontend.routes :as wr]
    [frontend.notifications.core :refer [add-notification]]))
 
 ; this is a clojurescript namespace
 ; functions that should be available to all goldly systems
-
-(defn nav [& args]
-  (rf/dispatch (into [] (concat [:bidi/goto] args))))
-
-(defn current-route []
-  @wr/current)
 
 (defn dialog [f & [size]]
   (if size
